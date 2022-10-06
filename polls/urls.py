daceb6424f9c88ffb8069774750bb91b0af703e5
dasbairagya <dasbairagyagopal@gmail.com>
2022-10-06 12:11:59 +0530

    Simple view via functional approach

---
+++ polls/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+#the polls app has a detail view, and so might an app on the same project that is for a blog.
+#which app view to create for a url when using the {% url %} template tag? for this create a namespace with polls
+app_name = 'polls' #<- creating namespace for polls app @see index.html
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    # the 'name' value as called by the {% url %} template tag
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
