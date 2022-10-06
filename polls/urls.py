9d85b10e899f076f10dfd1f11e1936918a7ad7c7
dasbairagya <dasbairagyagopal@gmail.com>
2022-10-06 14:54:08 +0530

    ListView and DetailsView via Class approach

--- polls/urls.py
+++ polls/urls.py
@@ -5,14 +5,21 @@ from . import views
 #which app view to create for a url when using the {% url %} template tag? for this create a namespace with polls
 app_name = 'polls' #<- creating namespace for polls app @see index.html
 
+# urlpatterns = [
+#     # ex: /polls/
+#     path('', views.index, name='index'),
+#     # ex: /polls/5/
+#     # the 'name' value as called by the {% url %} template tag
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    # the 'name' value as called by the {% url %} template tag
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
